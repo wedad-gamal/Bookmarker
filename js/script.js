c223d90239721a7dd36735e0b8a9b702c736cc2b
Wedad <wedad.gamal@gmail.com>
2023-12-27 21:32:00 +0200

    add remaing cruds operation (search & update)

--- js/script.js
+++ js/script.js
@@ -3,23 +3,25 @@ var siteUrl = document.getElementById("siteUrl");
 var siteNameError = document.getElementById("siteNameError");
 var siteURLError = document.getElementById("siteURLError");
 var submitBtn = document.getElementById("submitBtn");
-var tableData = document.getElementById('tableData');
-
+var updateBtn = document.getElementById("updateBtn");
+var tableData = document.getElementById("tableData");
+var searchTextBox = document.getElementById("searchTextBox");
 var siteList = [];
 
-if(siteList.length == 0){
-    var list = localStorage.getItem('siteList');
-    siteList = JSON.parse(list);
-    display();
+if (siteList.length == 0) {
+  var list = localStorage.getItem("siteList");
+  siteList = JSON.parse(list);
+  display();
 }
 
 submitBtn.onclick = function () {
-  addSite();
+  addOrUpdateSite();
 };
 
 siteName.onkeyup = function () {
   isSiteNameValid();
 };
+
 function isSiteNameValid() {
   var regex = /^\w{3,}$/;
   return isValid(
@@ -33,6 +35,7 @@ function isSiteNameValid() {
 siteUrl.onkeyup = function () {
   isSiteURLValid();
 };
+
 function isSiteURLValid() {
   var regex =
     /(https:\/\/www\.|http:\/\/www\.|https:\/\/|http:\/\/)?[a-zA-Z0-9]{2,}(\.[a-zA-Z0-9]{2,})(\.[a-zA-Z0-9]{2,})?/;
@@ -49,7 +52,7 @@ function isValid(regex, textbox, errorObject, errorMessage) {
   if (result == false) {
     errorObject.innerHTML = errorMessage;
     textbox.classList.add("is-invalid");
-    textbox.classList.remove("is-valid");    
+    textbox.classList.remove("is-valid");
   } else {
     errorObject.innerHTML = "";
     textbox.classList.add("is-valid");
@@ -58,71 +61,112 @@ function isValid(regex, textbox, errorObject, errorMessage) {
   return result;
 }
 
-function addSite() {
-
-  if (isSiteNameValid() && isSiteURLValid() ) {  
+function addOrUpdateSite(index) {
+  if (isSiteNameValid() && isSiteURLValid()) {
     var site = {
-        name: siteName.value,
-        url: siteUrl.value,
-      };
-      console.log("site ", site);
-    
+      name: siteName.value,
+      url: siteUrl.value,
+    };
+    console.log("site ", site);
+
+    if (index >= 0) {
+      siteList.splice(index, 1, site);
+    } else {
       siteList.push(site);
-      console.log("site list ", siteList);
-      localStorage.setItem("siteList", JSON.stringify(siteList));            
-      fireSuccessAddSweetAlert();
-      clearForm();
-      display();
-  }else{    
-    console.log( `name: ${siteName.value},
-        url: ${siteUrl.value}`);
-    console.log('is not valid data');  
+    }
+
+    console.log("site list ", siteList);
+    localStorage.setItem("siteList", JSON.stringify(siteList));
+    fireSuccessAddSweetAlert();
+    clearForm();
+    display();
+  } else {
     fireErrorSweetAlert();
-    return;
   }
 }
 
+function updateSite(index) {
+  siteName.value = siteList[index].name;
+  siteUrl.value = siteList[index].url;
+  updateBtn.classList.remove("d-none");
+  submitBtn.classList.add("d-none");
 
-function display(){
-    var box ='';
-    for(var i=0; i< siteList.length; i++){
-        box +=`
-        <tr>
-        <td>${i+1}</td>
-        <td>${siteList[i].name}</td>
-        <td>
-          <a href='${siteList[i].url}' class="btn btn-success" target="_blank">
-            <i class="fa-solid fa-eye"></i> Visit
-          </a>
-        </td>
-        <td>
-          <button class="btn btn-danger" onclick='deleteSite(${i})'>
-            <i class="fa-solid fa-trash-can"></i> Delete
-          </button>
-        </td>
-      </tr>
-        `;
+  updateBtn.onclick = function () {
+    addOrUpdateSite(index);
+  };
+}
+
+function display(searchValue) {
+  var box = "";
+  var isSearchBy = typeof searchValue !== "undefined" && searchValue.length > 0;
+
+  for (var i = 0; i < siteList.length; i++) {
+    var name = isSearchBy
+      ? siteList[i].name.replace(
+          searchValue,
+          `<span style="color: #d28006;">${searchValue}</span>`
+        )
+      : siteList[i].name;
+
+    if (
+      typeof searchValue == "undefined" ||
+      searchValue.length == 0 ||
+      (typeof searchValue !== "undefined" &&
+        siteList[i].name.includes(searchValue))
+    ) {
+      box += `
+          <tr>
+          <td>${i + 1}</td>
+          <td>${name}</td>
+          <td>
+            <a href='${
+              siteList[i].url
+            }' class="btn btn-success" target="_blank">
+              <i class="fa-solid fa-eye"></i> Visit
+            </a>
+          </td>
+          
+          <td>
+            <button class="btn btn-primary" onclick='updateSite(${i})'>
+            <i class="fa-solid fa-pen-to-square"></i> Update
+            </button>
+          </td>
+          <td>
+            <button class="btn btn-danger" onclick='deleteSite(${i})'>
+              <i class="fa-solid fa-trash-can"></i> Delete
+            </button>
+          </td>
+        </tr>
+          `;
     }
-    tableData.innerHTML = box;
+  }
+  tableData.innerHTML = box;
 }
 
-
-function deleteSite(index){
-    siteList.splice(index, 1);
-    display();
+searchTextBox.onkeyup = function () {
+  searchProcess();
+};
+function searchProcess() {
+  var searchValue = searchTextBox.value;
+  display(searchValue);
 }
 
-function clearForm(){
-    siteName.value='';
-    siteUrl.value='';    
-    siteName.classList.remove("is-valid");       
-    siteUrl.classList.remove("is-valid");   
+function deleteSite(index) {
+  siteList.splice(index, 1);
+  display();
 }
 
-function fireErrorSweetAlert(){
-    Swal.fire({
-        title: "",
-        html: `
+function clearForm() {
+  siteName.value = "";
+  siteUrl.value = "";
+  siteName.classList.remove("is-valid");
+  siteUrl.classList.remove("is-valid");
+}
+
+function fireErrorSweetAlert() {
+  Swal.fire({
+    title: "",
+    html: `
           <div class="text-start fs-6">
           Site Name or Url is not valid, Please follow the rules below :
       
@@ -130,16 +174,16 @@ function fireErrorSweetAlert(){
           <div><i class="fa-regular fa-circle-right text-danger me-2"></i>Site URL must be a valid one</div>
           </div>
           `,
-        showCloseButton: true,
-      });
+    showCloseButton: true,
+  });
 }
 
-function fireSuccessAddSweetAlert(){
-    Swal.fire({
-        position: "center",
-        icon: "success",
-        title: "Site has been saved",
-        showConfirmButton: false,
-        timer: 1500
-      });
+function fireSuccessAddSweetAlert() {
+  Swal.fire({
+    position: "center",
+    icon: "success",
+    title: "Site has been saved",
+    showConfirmButton: false,
+    timer: 1500,
+  });
 }
